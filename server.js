7b86ffc7ccbffabb93be9076a0d652122b904f4a
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-13 17:38:19 -0300

    configure with express

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const restify = require('restify');
-const cors = require('cors');
-const porta = 3000
-const bodyParser = require('body-parser')
-const servidor = restify.createServer()
-const transactionsData = require('./resources/transactions');
-const assetsData = require('./resources/assets');
+var express = require('express')
+var app = express()
+var transactionsData = require('./resources/transactions');
+var assetsData = require('./resources/assets');
 
-servidor.use(restify.plugins.bodyParser({ "params": true })); servidor.use(cors.actual);
-
-servidor.get('/transactions', (req, res) => {
-    res.send(transactionsData);
-});
-
-servidor.get('/assets', (req, res) => {
-    res.send(assetsData);
+app.get('/', function (req, res) {
+    res.send('Hello World!')
 })
 
-servidor.listen(porta, () => {
-    console.log(`Servidor de pé em http://localhost:${porta}`)
-    console.log('Pra derrubar o servidor: ctrl + c')
-});
+app.get('/transactions', (req, res) => {
+    res.send(transactionsData)
+})
+
+app.get('/assets', (req, res) => {
+    res.send(assetsData)
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
