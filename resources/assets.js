2cb0f650b48da380f21969ec3ca463a2637929a0
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-13 17:16:47 -0300

    testes with body-parsers

---
+++ resources/assets.js
@@ -0,0 +1,330 @@
+module.exports = {
+    "data": [
+        {
+            "id": "1",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11110",
+                "asset_description": "Tesouro IPCA+ 2024",
+                "selic_code": 10000,
+                "isin_code": "22220",
+                "business_code": "1",
+                "base_value": "2212.22",
+                "purchase_value": "44.24",
+                "purchase_rate_value": "5.5",
+                "sale_value": "2196.76",
+                "sale_rate_value": "5.62",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2024-08-15 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "2",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11111",
+                "asset_description": "Tesouro IPCA+ 2035",
+                "selic_code": 10001,
+                "isin_code": "22221",
+                "business_code": "1",
+                "base_value": "1171.48",
+                "purchase_value": "35.14",
+                "purchase_rate_value": "5.89",
+                "sale_value": "1149.3",
+                "sale_rate_value": "6.01",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2035-05-15 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "3",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11112",
+                "asset_description": "Tesouro IPCA+ 2045",
+                "selic_code": 10002,
+                "isin_code": "22222",
+                "business_code": "1",
+                "base_value": "662.03",
+                "purchase_value": "33.1",
+                "purchase_rate_value": "5.89",
+                "sale_value": "642.19",
+                "sale_rate_value": "6.01",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2045-05-15 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "4",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11113",
+                "asset_description": "Tesouro IPCA+ com Juros Semestrais 2026",
+                "selic_code": 10003,
+                "isin_code": "22223",
+                "business_code": "1",
+                "base_value": "3224.45",
+                "purchase_value": "32.24",
+                "purchase_rate_value": "5.55",
+                "sale_value": "3200.71",
+                "sale_rate_value": "5.67",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2026-08-15 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "5",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11114",
+                "asset_description": "Tesouro IPCA+ com Juros Semestrais 2035",
+                "selic_code": 10004,
+                "isin_code": "22224",
+                "business_code": "1",
+                "base_value": "3169.65",
+                "purchase_value": "31.69",
+                "purchase_rate_value": "5.77",
+                "sale_value": "3130.66",
+                "sale_rate_value": "5.89",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2035-05-15 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "6",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11115",
+                "asset_description": "Tesouro IPCA+ com Juros Semestrais 2050",
+                "selic_code": 10005,
+                "isin_code": "22225",
+                "business_code": "1",
+                "base_value": "3202.93",
+                "purchase_value": "32.02",
+                "purchase_rate_value": "5.86",
+                "sale_value": "3150.55",
+                "sale_rate_value": "5.98",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2050-08-15 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "7",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11116",
+                "asset_description": "Tesouro Prefixado 2021",
+                "selic_code": 10006,
+                "isin_code": "22226",
+                "business_code": "1",
+                "base_value": "799.24",
+                "purchase_value": "31.96",
+                "purchase_rate_value": "9.12",
+                "sale_value": "796.99",
+                "sale_rate_value": "9.24",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2021-01-01 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "8",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11117",
+                "asset_description": "Tesouro Prefixado 2025",
+                "selic_code": 10007,
+                "isin_code": "22227",
+                "business_code": "1",
+                "base_value": "479.63",
+                "purchase_value": "33.57",
+                "purchase_rate_value": "11.86",
+                "sale_value": "476.27",
+                "sale_rate_value": "11.98",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2025-01-01 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "9",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11118",
+                "asset_description": "Tesouro Prefixado com Juros Semestrais 2029",
+                "selic_code": 10008,
+                "isin_code": "22228",
+                "business_code": "1",
+                "base_value": "923.56",
+                "purchase_value": "36.94",
+                "purchase_rate_value": "12.13",
+                "sale_value": "917.42",
+                "sale_rate_value": "12.25",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2029-01-01 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        },
+        {
+            "id": "10",
+            "type": "assets",
+            "attributes": {
+                "asset_code": "11119",
+                "asset_description": "Tesouro Selic 2023",
+                "selic_code": 10009,
+                "isin_code": "22229",
+                "business_code": "1",
+                "base_value": "9532.06",
+                "purchase_value": "95.32",
+                "purchase_rate_value": "0.02",
+                "sale_value": "9514.09",
+                "sale_rate_value": "0.06",
+                "issue_date": "2000-01-01 00:00:00 +0000",
+                "due_date": "2023-03-01 00:00:00 +0000",
+                "created_at": "2018-06-07 14:22:37 +0000",
+                "updated_at": "2018-06-07 14:22:37 +0000"
+            },
+            "relationships": {
+                "asset_type": {
+                    "meta": {
+                        "included": false
+                    }
+                },
+                "asset_indexer": {
+                    "meta": {
+                        "included": false
+                    }
+                }
+            }
+        }
+    ],
+    "meta": {
+        "total-count": 10
+    },
+    "jsonapi": {
+        "version": "1.0"
+    }
+}
