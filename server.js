138d5a5ce3577c1a9eefdaac58b0dec430c06509
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-15 12:51:00 -0300

    config post response

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
-var require = require('request');
+var bodyParser = require('body-parser')
 
 //Url's data
 var transactionsData = require('./resources/transactions');
@@ -9,6 +9,8 @@ var assetsData = require('./resources/assets');
 var statuses = require('./resources/statuses');
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('start')
@@ -24,6 +26,11 @@ app.get('/transactions/statuses', (req, res) => {
 
 app.get('/assets', (req, res) => {
     res.send(assetsData)
+})
+
+app.post('/xablau', (req, res) => {
+    var corpo = req.body;
+    res.send(console.log(corpo.name))
 })
 
 app.listen(3000, () => {
